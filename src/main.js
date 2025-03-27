7fab015b8076c2f7acf169e01c8d94432f09b814
Mark Visschers <visschers.m@gmail.com>
2025-03-28 00:19:13 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import Reveal from "reveal.js";
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
+
+Reveal.initialize({ plugins: [Markdown, Highlight] });
