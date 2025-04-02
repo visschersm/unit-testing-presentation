47daf126f3a984d98fe1feea8d07252766b30562
Mark Visschers <visschers.m@gmail.com>
2025-04-02 20:35:57 +0200

    adds dataprocessinglib for demo

---
+++ demos/DataProcessingLibrary/DataProcessingService.cs
@@ -0,0 +1,18 @@
+﻿namespace DataProcessingLibrary;
+
+public class DataProcessingLibrary
+{
+    private readonly IHttpClient _client;
+    private readonly IDbContext context;
+    public DataProcessingLibrary(IHttpClientFactory factory, IDbContext context, ILogger logger)
+    {
+        _client = factory.CreateClient();
+        _context = context;
+        _logger = logger;
+    }
+
+    public Task ProcessData()
+    {
+        return Task.FromResult(0);
+    }
+}
