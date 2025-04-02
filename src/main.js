06a0cd3f954a2471616c0269e93595cd5dc2a52e
Mark Visschers <visschers.m@gmail.com>
2025-04-02 20:34:33 +0200

    wip: demo and altering slides

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
 import RevealMermaid from 'reveal.js-mermaid-plugin/plugin/mermaid/mermaid.js';
 
-Reveal.initialize({ plugins: [Markdown, Highlight, RevealMermaid] });
+Reveal.initialize({
+    plugins: [Markdown, Highlight, RevealMermaid],
+    markdown: {
+        html: true
+    }
+});
