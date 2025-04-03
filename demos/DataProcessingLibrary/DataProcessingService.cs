40c219e9f9352042900589f5a67f4e01e0aba50e
Mark Visschers <visschers.m@gmail.com>
2025-04-03 12:55:31 +0200

    finishing up tests

--- demos/DataProcessingLibrary/DataProcessingService.cs
+++ demos/DataProcessingLibrary/DataProcessingService.cs
@@ -1,10 +1,15 @@
 ﻿namespace DataProcessingLibrary;
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
 public class DataProcessingLibrary
 {
-    private readonly IHttpClient _client;
-    private readonly IDbContext context;
-    public DataProcessingLibrary(IHttpClientFactory factory, IDbContext context, ILogger logger)
+    private readonly HttpClient _client;
+    private readonly DbContext _context;
+    private readonly ILogger _logger;
+    
+    public DataProcessingLibrary(IHttpClientFactory  factory, DbContext context, ILogger logger)
     {
         _client = factory.CreateClient();
         _context = context;
---
+++ demos/MockingDependencies/Test1.cs
@@ -0,0 +1,148 @@
+﻿using System.Reflection;
+
+namespace MockingDependencies;
+
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void AdvanceAddition_ValidState_StatusCode200()
+    {
+        var dependency1 = new ExternalDependencyImplementation();
+        
+        var sut = new MathService1(dependency1);
+
+        var result = sut.AdvanceAddition(1, 2);
+
+        Assert.AreEqual(200, result.StatusCode);
+        Assert.AreEqual(3, result.Data);
+    }
+
+    [TestMethod]
+    public void AdvanceAddition_InternalServerError_StatusCode500_ByOverride()
+    {
+        var dependency1 = new ExternalDepenedencyOverrideImplementation
+        {
+            ThrowException = true,
+        };
+
+        var sut = new MathService1(dependency1);
+        
+        var result = sut.AdvanceAddition(1, 2);
+
+        Assert.AreEqual(500, result.StatusCode);
+        Assert.AreEqual(null, result.Data);
+    }
+    
+    // [TestMethod]
+    // public void AdvanceAddition_InternalServerError_StatusCode500_ByHiding()
+    // {
+    //     var dependency1 = new ExternalDepenedencyHidingImplementation
+    //     {
+    //         ThrowException = true,
+    //     };
+    //
+    //     var sut = new MathService1(dependency1);
+    //     
+    //     var result = sut.AdvanceAddition(1, 2);
+    //
+    //     Assert.AreEqual(500, result.StatusCode);
+    //     Assert.AreEqual(null, result.Data);
+    // }
+    
+    [TestMethod]
+    public void AdvanceAddition_InternalServerError_StatusCode500_BySubstitute()
+    {
+        var dependency1 = Substitute.For<ExternalDependencyImplementation>();
+        dependency1.DoStuff(Arg.Any<int>(), Arg.Any<int>())
+            .Throws(new Exception());
+        
+        var sut = new MathService1(dependency1);
+        
+        var result = sut.AdvanceAddition(1, 2);
+
+        Assert.AreEqual(500, result.StatusCode);
+        Assert.AreEqual(null, result.Data);
+    }
+    
+    public interface IExernalDependency1
+    {
+        int DoStuff(int x, int y);
+    }
+
+    public class MathService1
+    {
+        private readonly ExternalDependencyImplementation _dependency1;
+        
+        public MathService1(ExternalDependencyImplementation dependency1)
+        {
+            _dependency1 = dependency1;
+        }
+
+        public Response AdvanceAddition(int x, int y)
+        {
+            try
+            {
+                var result = _dependency1.DoStuff(x, y);
+                return new Response
+                {
+                    StatusCode = 200,
+                    Data = result,
+                };
+            }
+            catch
+            {
+                return new Response
+                {
+                    StatusCode = 500,
+                    Data = null,
+                };
+            }
+        }
+    }
+
+    public class Response
+    {
+        public int StatusCode { get; set; }
+        public int? Data { get; set; }
+    }
+
+    public class ExternalDependencyImplementation
+    {
+        public virtual int DoStuff(int x, int y)
+        {
+            return x + y;
+        }
+    }
+
+    public class ExternalDepenedencyOverrideImplementation : ExternalDependencyImplementation
+    {
+        public bool ThrowException { get; set; } = false;
+        public override int DoStuff(int x, int y)
+        {
+            if (ThrowException)
+            {
+                throw new Exception();
+            }
+
+            return x + y;
+        }
+    }
+    
+    public class ExternalDepenedencyHidingImplementation : IExernalDependency1
+    {
+        public bool ThrowException { get; set; } = false;
+        public new int DoStuff(int x, int y)
+        {
+            if (ThrowException)
+            {
+                throw new Exception();
+            }
+
+            return x + y;
+        }
+    }
+}
